907fbae89a4e8fb1026416ca422426107de1e950
LinJK <hodi88780838@163.com>
2017-06-17 10:19:46 +0800

    first commit

---
+++ test1/test1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  test1
+//
+//  Created by hodi on 16/06/2017.
+//  Copyright © 2017 LiDC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        NSLog(@"你好，我是大东");
+        int a = 12;
+        a++;
+        NSLog(@"%d",a++);
+        NSLog(@"%d",++a);
+    }
+    return 0;
+}
