fa4e62d8a36f17aedab7afe3a1f7c8f334acc2f8
LinJK <hodi88780838@163.com>
2017-06-18 10:33:41 +0800

    objective-c 基础

--- test1/test1/main.m
+++ test1/test1/main.m
@@ -8,6 +8,12 @@
 
 #import <Foundation/Foundation.h>
 
+double area(){
+    int a=12;
+    int b=4;
+    return a*b;
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
@@ -17,6 +23,43 @@ int main(int argc, const char * argv[]) {
         a++;
         NSLog(@"%d",a++);
         NSLog(@"%d",++a);
+        if (a<13) {
+            NSLog(@"a小于13");
+        }else{
+            NSLog(@"a大于等于13");
+        }
+        int i;
+        //高级跳转语句
+    a:
+        i++;
+        NSLog(@"i的值%d",i);
+        if (i<5) {
+            goto a;
+        }
+        int n;
+        while (n<5) {
+            n++;
+            NSLog(@"n的值%d",n);
+        }
+        for (int i=0; i<10; i++) {
+            if (i==5) {
+                continue;//跳出本次执行，继续执行下一个
+            }
+            if (i==7) {
+                break;//跳出整个循环
+            }
+            NSLog(@"%d",i);
+        }
+        
+        double s = area();
+        NSLog(@"面积为%.2f",s);
+        
+        //main函数的参数
+        NSLog(@"argc=%d",argc);
+        for (int j=0; j<argc; j++) {
+            NSLog(@"argv[%d]=%s",j,argv[j]);
+        }
+        
     }
     return 0;
 }
